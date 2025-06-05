5b1d76a39087f2c653213086abb1c709f3f1bac6
Evan Almloff <evanalmloff@gmail.com>
2025-06-05 16:42:53 -0500

    style slider

--- preview/src/components/slider/mod.rs
+++ preview/src/components/slider/mod.rs
@@ -4,38 +4,17 @@ use dioxus_primitives::slider::{
 };
 #[component]
 pub(super) fn Demo() -> Element {
-    let mut value = use_signal(|| SliderValue::Single(50.0));
     rsx! {
         document::Link {
             rel: "stylesheet",
             href: asset!("/src/components/slider/style.css"),
         }
-        div { class: "slider-example",
-            div {
-                label { "Single Value Slider" }
-                div { style: "display: flex; flex-wrap: wrap; align-items: center; gap: 1rem;",
-                    Slider {
-                        class: "slider",
-                        value,
-                        horizontal: true,
-                        on_value_change: move |v| {
-                            value.set(v);
-                        },
-                        SliderTrack { class: "slider-track",
-                            SliderRange { class: "slider-range" }
-                            SliderThumb { class: "slider-thumb" }
-                        }
-                    }
-                    input {
-                        class: "slider-value",
-                        r#type: "text",
-                        readonly: true,
-                        value: match value() {
-                            SliderValue::Single(v) => format!("{v:.1}"),
-                            _ => String::new(),
-                        },
-                    }
-                }
+        Slider {
+            class: "slider",
+            horizontal: true,
+            SliderTrack { class: "slider-track",
+                SliderRange { class: "slider-range" }
+                SliderThumb { class: "slider-thumb" }
             }
         }
     }
