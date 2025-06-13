6a306809305d4a540804d61efd042ba10039adc0
Evan Almloff <evanalmloff@gmail.com>
2025-06-12 19:41:57 -0500

    longer goto timeout

--- playwright/preview.spec.ts
+++ playwright/preview.spec.ts
@@ -5,7 +5,7 @@ test.describe("homepage", () => {
   test("should not have any automatically detectable accessibility issues", async ({
     page,
   }) => {
-    await page.goto("http://127.0.0.1:8080/");
+    await page.goto("http://127.0.0.1:8080/", { timeout: 1200000 }); // Increase timeout to 20 minutes
 
     // Wait for the page to fully load
     let heroSection = page.locator("#hero");
