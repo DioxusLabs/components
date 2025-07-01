b80013ffa91ff73dc8136044e6e317a5daf6f72a
Evan Almloff <evanalmloff@gmail.com>
2025-07-01 15:39:56 -0500

    document separator

--- primitives/src/separator.rs
+++ primitives/src/separator.rs
@@ -1,8 +1,8 @@
+//! Defines the [`Separator`] component for creating visual or semantic separators.
+
 use dioxus_lib::prelude::*;
 
-// TODO: Docs
-
-/// The props for the [`Separator`] component
+/// The props for the [`Separator`] component.
 #[derive(Props, Clone, PartialEq)]
 pub struct SeparatorProps {
     /// Horizontal if true, vertical if false.
@@ -14,12 +14,42 @@ pub struct SeparatorProps {
     #[props(default = false)]
     decorative: bool,
 
+    /// Additional attributes to apply to the separator element.
     #[props(extends = GlobalAttributes)]
     attributes: Vec<Attribute>,
 
+    /// The children of the separator component.
     children: Element,
 }
 
+/// # Separator
+///
+/// The `Separator` component creates a visual or semantic divider between sections of content. If the divider
+/// is purely decorative, it can be marked as such to avoid being classified as a separator by assistive technologies.
+///
+/// ## Example
+///
+/// ```rust
+/// use dioxus::prelude::*;
+/// use dioxus_primitives::separator::Separator;
+/// #[component]
+/// fn Demo() -> Element {
+///     rsx! {
+///         "One thing"
+///         Separator {
+///             style: "margin: 15px 0; width: 50%;",
+///             horizontal: true,
+///             decorative: true,
+///         }
+///         "Another thing"
+///     }
+/// }
+/// ```
+///
+/// ## Styling
+///
+/// The [`Separator`] component defines the following data attributes you can use to control styling:
+/// - `data-orientation`: Indicates the orientation of the separator. Values are `horizontal` or `vertical`.
 #[component]
 pub fn Separator(props: SeparatorProps) -> Element {
     let orientation = match props.horizontal {
