21964183e1f12f6e1d38fd6d6cca06c2eca2e406
Evan Almloff <evanalmloff@gmail.com>
2025-07-11 14:22:04 -0500

    test hovercard and tooltip

---
+++ playwright/tooltip.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://127.0.0.1:8080/component/?name=tooltip&");
+  let tooltip = page.getByRole("tooltip");
+  // tabbing to the trigger element should show the tooltip
+  await page.getByRole("heading", { name: "tooltip" }).click();
+  await page.keyboard.press("Tab");
+  await expect(tooltip).toBeVisible();
+  // tabbing out of the trigger element should hide the tooltip
+  await page.keyboard.press("Tab");
+  await expect(tooltip).toHaveCount(0);
+
+  // hovering over the trigger element should show the tooltip
+  await page.locator(".tooltip-trigger").hover();
+  await expect(tooltip).toBeVisible();
+
+  // moving the mouse away from the trigger element should hide the tooltip
+  await page.mouse.move(0, 0);
+  await expect(tooltip).toHaveCount(0);
+});
