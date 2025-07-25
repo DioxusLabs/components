82ac11cc89cd49f8a1bca91d9459f78eb37b8439
Patsakula Nikita <nikita.patsakula@gmail.com>
2025-07-25 14:26:04 +0300

    [-] Select: cargofmt.

--- primitives/src/select/components/mod.rs
+++ primitives/src/select/components/mod.rs
@@ -1,13 +1,13 @@
 //! Component definitions for the select primitive.
 
-pub mod select;
-pub mod trigger;
+pub mod group;
 pub mod list;
 pub mod option;
-pub mod group;
+pub mod select;
+pub mod trigger;
 
+pub use group::{SelectGroup, SelectGroupLabel, SelectGroupLabelProps, SelectGroupProps};
+pub use list::{SelectList, SelectListProps};
+pub use option::{SelectItemIndicator, SelectItemIndicatorProps, SelectOption, SelectOptionProps};
 pub use select::{Select, SelectProps};
 pub use trigger::{SelectTrigger, SelectTriggerProps};
-pub use list::{SelectList, SelectListProps};
-pub use option::{SelectOption, SelectOptionProps, SelectItemIndicator, SelectItemIndicatorProps};
-pub use group::{SelectGroup, SelectGroupProps, SelectGroupLabel, SelectGroupLabelProps};
