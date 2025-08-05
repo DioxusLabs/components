da5f4c213fc3fb65e22e9747db6d939460b7b09e
Patsakula Nikita <nikita.patsakula@gmail.com>
2025-08-05 17:41:59 +0300

    [*] Typed `DropdownMenu` implemented. (#102)

--- preview/src/components/dropdown_menu/variants/main/mod.rs
+++ preview/src/components/dropdown_menu/variants/main/mod.rs
@@ -2,8 +2,33 @@ use dioxus::prelude::*;
 use dioxus_primitives::dropdown_menu::{
     DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,
 };
+use strum::IntoEnumIterator;
+
+#[derive(Clone, Copy, strum::Display, strum::EnumIter, PartialEq)]
+enum Operation {
+    Edit,
+    Undo,
+    Duplicate,
+    Delete,
+}
+
 #[component]
 pub fn Demo() -> Element {
+    let operations = Operation::iter().enumerate().map(|(i, o)| {
+        rsx! {
+            DropdownMenuItem::<Operation> {
+                class: "dropdown-menu-item",
+                value: o,
+                index: i,
+                disabled: matches!(o, Operation::Undo),
+                on_select: move |value| {
+                    tracing::info!("Selected: {value}");
+                },
+                {o.to_string()}
+            }
+        }
+    });
+
     rsx! {
         document::Link {
             rel: "stylesheet",
@@ -12,43 +37,7 @@ pub fn Demo() -> Element {
         DropdownMenu { class: "dropdown-menu", default_open: false,
             DropdownMenuTrigger { class: "dropdown-menu-trigger", "Open Menu" }
             DropdownMenuContent { class: "dropdown-menu-content",
-                DropdownMenuItem {
-                    class: "dropdown-menu-item",
-                    value: "edit".to_string(),
-                    index: 0usize,
-                    on_select: move |value| {
-                        tracing::info!("Selected: {}", value);
-                    },
-                    "Edit"
-                }
-                DropdownMenuItem {
-                    class: "dropdown-menu-item",
-                    value: "undo".to_string(),
-                    index: 1usize,
-                    disabled: true,
-                    on_select: move |value| {
-                        tracing::info!("Selected: {}", value);
-                    },
-                    "Undo"
-                }
-                DropdownMenuItem {
-                    class: "dropdown-menu-item",
-                    value: "duplicate".to_string(),
-                    index: 2usize,
-                    on_select: move |value| {
-                        tracing::info!("Selected: {}", value);
-                    },
-                    "Duplicate"
-                }
-                DropdownMenuItem {
-                    class: "dropdown-menu-item",
-                    value: "delete".to_string(),
-                    index: 3usize,
-                    on_select: move |value| {
-                        tracing::info!("Selected: {}", value);
-                    },
-                    "Delete"
-                }
+                {operations}
             }
         }
     }
