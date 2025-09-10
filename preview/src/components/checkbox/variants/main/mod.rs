c755434266d47e1444b95d39fb5317a3823782c3
Evan Almloff <evanalmloff@gmail.com>
2025-09-10 09:44:35 -0500

    split out checkbox

--- preview/src/components/checkbox/variants/main/mod.rs
+++ preview/src/components/checkbox/variants/main/mod.rs
@@ -1,5 +1,6 @@
+use super::super::component::*;
 use dioxus::prelude::*;
-use dioxus_primitives::checkbox::{Checkbox, CheckboxIndicator};
+
 #[component]
 pub fn Demo() -> Element {
     rsx! {
@@ -8,18 +9,8 @@ pub fn Demo() -> Element {
             href: asset!("/src/components/checkbox/variants/main/style.css"),
         }
         Checkbox {
-            class: "checkbox",
             name: "tos-check",
             aria_label: "Demo Checkbox",
-            CheckboxIndicator {
-                class: "checkbox-indicator",
-                svg {
-                    class: "checkbox-check-icon",
-                    view_box: "0 0 24 24",
-                    xmlns: "http://www.w3.org/2000/svg",
-                    path { d: "M5 13l4 4L19 7" }
-                }
-            }
         }
     }
 }
