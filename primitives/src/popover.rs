85638446cf4d85e1368035fe674238ad052d27bf
Evan Almloff <evanalmloff@gmail.com>
2025-10-20 09:16:18 -0500

    fix formatting

--- primitives/src/popover.rs
+++ primitives/src/popover.rs
@@ -25,7 +25,6 @@ struct PopoverCtx {
 /// The props for the [`PopoverRoot`] component.
 #[derive(Props, Clone, PartialEq)]
 pub struct PopoverRootProps {
-    
     /// Whether the popover is a modal and should capture focus.
     #[props(default = ReadSignal::new(Signal::new(true)))]
     pub is_modal: ReadSignal<bool>,
