a80356f95b15de18b4dd8cb643dd46952de5f733
Evan Almloff <evanalmloff@gmail.com>
2025-10-22 17:21:20 -0500

    add oninput handler to example

--- preview/src/components/input/variants/main/mod.rs
+++ preview/src/components/input/variants/main/mod.rs
@@ -3,7 +3,15 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Demo() -> Element {
+    let mut name = use_signal(String::new);
     rsx! {
-        Input { placeholder: "Enter your name" }
+        Input {
+            oninput: move |e: FormEvent| name.set(e.value()),
+            placeholder: "Enter your name",
+            value: name,
+        }
+        if !name.read().is_empty() {
+            p { "Hello, {name}!" }
+        }
     }
 }
