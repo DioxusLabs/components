fc8142894d7c3bf4cd48e29aa1cb042c997783d5
Evan Almloff <evanalmloff@gmail.com>
2026-05-07 10:40:08 -0500

    match shadcn more closely

--- preview/src/components/button/variants/size/mod.rs
+++ preview/src/components/button/variants/size/mod.rs
@@ -5,62 +5,81 @@ use dioxus_primitives::icon::Icon;
 #[component]
 pub fn Demo() -> Element {
     rsx! {
-        div {
-            display: "flex",
-            flex_direction: "row",
-            flex_wrap: "wrap",
-            align_items: "flex-start",
-            gap: "2rem",
+        style { {SIZE_DEMO_STYLE} }
+        div { class: "dx-button-size-demo",
 
             div {
-                display: "flex", align_items: "flex-start", gap: "0.5rem",
+                display: "flex",
+                align_items: "flex-start",
+                gap: "0.5rem",
                 Button { size: ButtonSize::Xs, variant: ButtonVariant::Outline, "Extra Small" }
                 Button {
                     size: ButtonSize::IconXs,
                     variant: ButtonVariant::Outline,
                     aria_label: "Submit",
-                    ArrowUpRightIcon { width: "12px", height: "12px" }
+                    ArrowUpRightIcon {}
                 }
             }
 
             div {
-                display: "flex", align_items: "flex-start", gap: "0.5rem",
+                display: "flex",
+                align_items: "flex-start",
+                gap: "0.5rem",
                 Button { size: ButtonSize::Sm, variant: ButtonVariant::Outline, "Small" }
                 Button {
                     size: ButtonSize::IconSm,
                     variant: ButtonVariant::Outline,
                     aria_label: "Submit",
-                    ArrowUpRightIcon { width: "14px", height: "14px" }
+                    ArrowUpRightIcon {}
                 }
             }
 
             div {
-                display: "flex", align_items: "flex-start", gap: "0.5rem",
+                display: "flex",
+                align_items: "flex-start",
+                gap: "0.5rem",
                 Button { variant: ButtonVariant::Outline, "Default" }
                 Button {
                     size: ButtonSize::Icon,
                     variant: ButtonVariant::Outline,
                     aria_label: "Submit",
-                    ArrowUpRightIcon { width: "16px", height: "16px" }
+                    ArrowUpRightIcon {}
                 }
             }
 
             div {
-                display: "flex", align_items: "flex-start", gap: "0.5rem",
+                display: "flex",
+                align_items: "flex-start",
+                gap: "0.5rem",
                 Button { size: ButtonSize::Lg, variant: ButtonVariant::Outline, "Large" }
                 Button {
                     size: ButtonSize::IconLg,
                     variant: ButtonVariant::Outline,
                     aria_label: "Submit",
-                    ArrowUpRightIcon { width: "18px", height: "18px" }
+                    ArrowUpRightIcon {}
                 }
             }
         }
     }
 }
 
+const SIZE_DEMO_STYLE: &str = r#"
+.dx-button-size-demo {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2rem;
+}
+
+@media (width >= 40rem) {
+  .dx-button-size-demo {
+    flex-direction: row;
+  }
+}
+"#;
+
 #[component]
-fn ArrowUpRightIcon(#[props(extends = GlobalAttributes)] attributes: Vec<Attribute>) -> Element {
+fn ArrowUpRightIcon() -> Element {
     rsx! {
         Icon {
             path { d: "M7 17 17 7" }
